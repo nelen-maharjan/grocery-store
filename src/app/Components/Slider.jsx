42bbb0db8521d9cb1b2fb72d827893d5fc603ce6
Nelen Maharjan <neleen11@gmail.com>
2024-11-15 08:37:00 +0545

    slider-carousal

--- src/app/Components/Slider.jsx
+++ src/app/Components/Slider.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
 
-const Slider = ({sliderList}) => {
+const Slider = ({ sliderList }) => {
   return (
     <div>
-        
+      <Carousel>
+        <CarouselContent>
+          {sliderList.map((slider, index) => (
+            <CarouselItem key={index}>
+              <Image
+                src={
+                  process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                  slider?.image[0]?.url
+                }
+                className="w-full md:h-[400px] h-[200px] object-cover rounded-2xl"
+                width={1000}
+                height={400}
+                alt="slider"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Slider from "./Components/Slider";
 import GlobalApi from "./utils/GlobalApi";
 
 export default async function Home() {
   const sliderList = await GlobalApi.getSliders();
   return (
-    <div>
+    <div className="p-5 md:p-10 px-16">
       <Slider sliderList={sliderList} />
     </div>
   );
